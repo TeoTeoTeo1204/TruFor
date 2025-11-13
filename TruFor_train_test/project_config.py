63a67da489ef9df160744db71f8c9c63f98c678a
Xuan Thanh <lexuanthanh.iamteo@gmail.com>
2025-11-13 23:21:00 +0700

    Config for run GPU mode

---
+++ TruFor_train_test/project_config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+project_root = Path(__file__).parent
+
+dataset_paths = {
+    # Specify where are the roots of the datasets.
+    # 'FR'       : 'path/to/FR',
+    'IMD'      : '/home/vitris/XT/data/IMD2020',
+    # 'CA'       : 'path/to/CA',
+    # 'tampCOCO' : 'path/to/tampCOCO',
+    # 'compRAISE': 'path/to/compRAISE',
+}
